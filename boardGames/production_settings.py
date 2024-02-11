7adfa0c67a08db845b6c7e76935a2853bdefeb8b
Davide <davideghz@gmail.com>
2024-02-11 15:23:51 +0100

    production settings

--- boardGames/production_settings.py
+++ boardGames/production_settings.py
@@ -1,4 +1,6 @@
 import dj_database_url
-from django.conf.global_settings import DATABASES
+import environ
+from django.conf.global_settings import DATABASES, MIDDLEWARE
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware',)
