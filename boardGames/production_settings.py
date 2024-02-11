20ef7da69ffe0566d8c5dbd3fecfd8400c0887d1
Davide <davideghz@gmail.com>
2024-02-11 15:32:13 +0100

    dummy

--- boardGames/production_settings.py
+++ boardGames/production_settings.py
@@ -5,3 +5,4 @@ from django.conf.global_settings import DATABASES, MIDDLEWARE
 ALLOWED_HOSTS = ['.herokuapp.com']
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware',)
+
