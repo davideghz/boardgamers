3edddbfe186a39a67e13520eca49ecca07285b3b
Davide <davideghz@gmail.com>
2024-02-11 15:37:12 +0100

    allowed_hosts

--- boardGames/production_settings.py
+++ boardGames/production_settings.py
@@ -2,7 +2,6 @@ import dj_database_url
 import environ
 from django.conf.global_settings import DATABASES, MIDDLEWARE
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['boardgamers-b44b863a1d98.herokuapp.com']
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware',)
-
