bce6618a43ad058830a9739513b5b704373990af
Davide <davideghz@gmail.com>
2024-02-12 23:31:21 +0100

    settings

--- boardGames/settings.py
+++ boardGames/settings.py
@@ -139,3 +139,13 @@ if DEBUG:
     INTERNAL_IPS = ('127.0.0.1',)
     INSTALLED_APPS.append('debug_toolbar')
     MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware',)
+
+if ENV == 'prod':
+    # Quick-start development settings - unsuitable for production
+    # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+    try:
+        ALLOWED_HOSTS = ['boardgamers-b44b863a1d98.herokuapp.com']
+        DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+        MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
+    except ImportError:
+        pass
