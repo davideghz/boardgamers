af74a472f38d090f268f96fd6a60116b4b6739fa
Davide <davideghz@gmail.com>
2024-02-12 18:35:46 +0100

    settings

--- boardGames/production_settings.py
+++ boardGames/production_settings.py
@@ -11,4 +11,6 @@ ALLOWED_HOSTS = ['boardgamers-b44b863a1d98.herokuapp.com']
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware',]
 
+print(MIDDLEWARE)
+
 print('prod settings loaded')
