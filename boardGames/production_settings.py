b2ef5a5cdb6f7b55493579cc08e75fb74a20e02a
Davide <davideghz@gmail.com>
2024-03-02 15:44:29 +0100

    enabling email in production

--- boardGames/production_settings.py
+++ boardGames/production_settings.py
@@ -25,6 +25,6 @@ EMAIL_BACKEND = 'django_ses.SESBackend'
 AWS_SES_ACCESS_KEY_ID = env('AWS_SES_ACCESS_KEY_ID')
 AWS_SES_SECRET_ACCESS_KEY = env('AWS_SES_SECRET_ACCESS_KEY')
 AWS_SES_REGION_NAME = 'eu-west-1'
-AWS_SES_REGION_ENDPOINT = 'email.eu-west-1.amazonaws.com'
+AWS_SES_REGION_ENDPOINT = 'email-smtp.eu-west-1.amazonaws.com'
 
 print('prod settings loaded')
