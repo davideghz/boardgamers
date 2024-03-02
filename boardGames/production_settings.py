139148612a429120fe5381bb7d7114afa094674d
Davide <davideghz@gmail.com>
2024-03-02 15:24:12 +0100

    enabling email in production

--- boardGames/production_settings.py
+++ boardGames/production_settings.py
@@ -16,10 +16,15 @@ DOMAIN = "board-gamers.com"
 DOMAIN_PROTOCOL = 'https'
 DOMAIN_URL = DOMAIN_PROTOCOL + "://" + DOMAIN
 
+# S3
 AWS_STORAGE_BUCKET_NAME = 'boardgamers-prod-public'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-# GDAL_LIBRARY_PATH = ''
-# GEOS_LIBRARY_PATH = ''
+# SES
+EMAIL_BACKEND = 'django_ses.SESBackend'
+AWS_SES_ACCESS_KEY_ID = 'YOUR-ACCESS-KEY-ID'
+AWS_SES_SECRET_ACCESS_KEY = 'YOUR-SECRET-ACCESS-KEY'
+AWS_SES_REGION_NAME = 'eu-west-1'
+AWS_SES_REGION_ENDPOINT = 'email.eu-west-1.amazonaws.com'
 
 print('prod settings loaded')
