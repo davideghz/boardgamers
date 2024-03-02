88c375aae67ae2617319fe9c6df0252a25063590
Davide <davideghz@gmail.com>
2024-03-02 15:52:09 +0100

    enabling sentry in production

--- boardGames/production_settings.py
+++ boardGames/production_settings.py
@@ -1,10 +1,13 @@
 from .settings import *
 import dj_database_url
+import sentry_sdk
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 ALLOWED_HOSTS = ['board-gamers.com']
+
+# DATABASE
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 if DATABASES['default']['ENGINE'] == 'django.db.backends.postgresql':
@@ -26,5 +29,17 @@ AWS_SES_ACCESS_KEY_ID = env('AWS_SES_ACCESS_KEY_ID')
 AWS_SES_SECRET_ACCESS_KEY = env('AWS_SES_SECRET_ACCESS_KEY')
 AWS_SES_REGION_NAME = 'eu-west-1'
 AWS_SES_REGION_ENDPOINT = 'email-smtp.eu-west-1.amazonaws.com'
+
+# SENTRY
+sentry_sdk.init(
+    dsn="https://9e20218ac6979cd5bc4ef57c44082902@o4506841965133824.ingest.sentry.io/4506841968017408",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
 
 print('prod settings loaded')
