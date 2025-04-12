b7b4be61d998f12854a1cc5c7aa354ffc8b92de1
Davide <davideghz@gmail.com>
2025-04-13 00:02:20 +0200

    feat: create local_settings_template.py and ignore local_settings.py

---
+++ boardGames/local_settings_template.py
@@ -0,0 +1,15 @@
+# IMPORTANT
+# Create a new file named loca_settings.py and override settings
+
+from .settings import *
+
+print('dentro local_settings.py')
+
+# Geospatial libraries
+GDAL_LIBRARY_PATH = '/Applications/Postgres.app/Contents/Versions/16/lib/libgdal.dylib'
+GEOS_LIBRARY_PATH = os.path.join('/opt/homebrew/opt/geos/lib', 'libgeos_c.dylib')
+
+# Django Toolbar
+INTERNAL_IPS = ('127.0.0.1',)
+INSTALLED_APPS.append('debug_toolbar')
+MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware',)
