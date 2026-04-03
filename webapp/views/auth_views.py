a204ffa1606900de98e8b209c399fb0ff8055c7b
Davide <davideghz@gmail.com>
2026-04-03 16:38:29 +0200

    fix: redirect authenticated users away from signup page

--- webapp/views/auth_views.py
+++ webapp/views/auth_views.py
@@ -28,6 +28,11 @@ class SignupView(generic.CreateView, RedirectURLMixin):
     form_class = UserRegistrationForm
     template_name = 'auth/signup.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('account-index')
+        return super().dispatch(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['meta'] = Meta(
