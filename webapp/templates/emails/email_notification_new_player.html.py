4be02dea03d3088d1f44b0ddaea7f57a95ca09b2
Davide <davideghz@gmail.com>
2026-08-17 15:07:38 +0200

    chore: remove accidentally committed makemessages temp artifacts
    The 100 *.html.py / *.txt.py files were xgettext temp copies that
    makemessages normally deletes; a past interrupted run left them tracked.
    Regenerating the catalog cleaned them from the tree. Gitignore the
    patterns so they never get committed again.

--- webapp/templates/emails/email_notification_new_player.html.py
+++
@@ -1,9 +0,0 @@
-BBBB BBBB
-
- gettext(u'Hey')  X
- gettext(u'A new player joined the table')  XXX X
-
-BB BBBB gettext(u'Game:')  BBBBB
-
-
-XXXXXXXXXXXXXXXX
--- webapp/templates/emails/email_verification_code.html.py
+++
@@ -1,6 +0,0 @@
-BBBB BBBB
-
- gettext(u'Hey')  X
- gettext(u'Please click the link to confirm your email address and finish setting up your account') X 
-
-XXXXXXXXXXXXXXXX
